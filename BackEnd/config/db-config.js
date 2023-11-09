3ab29c9ec5a85626e20bd19cbb15411b34d13618
EpsilonOmegatron <adham852002@hotmail.com>
2023-11-09 16:59:29 +0200

    Made port customizable

--- BackEnd/config/db-config.js
+++ BackEnd/config/db-config.js
@@ -1,5 +1,6 @@
 module.exports = {
   HOST: "localhost",
+  PORT: "1234",
   USER: "admin",
   PASSWORD: "1234",
   DATABASE: "clinicapp",
--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const db = require(path.join(__dirname, "models"));
+const dbConfig = require(path.join(__dirname, "config", "db-config.js"));
 
 (async () => {
   await db.sequelize.sync();
 })();
 
-app.listen(1234);
+app.listen(dbConfig.PORT);
