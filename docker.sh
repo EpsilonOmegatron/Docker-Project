d5934e58537cede2a6e939a408095ee9c5e3d922
EpsilonOmegatron <adham852002@hotmail.com>
2023-12-01 21:53:27 +0200

    Finished Dockerfiles + bash script

---
+++ docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker network create private
+docker build -t clinicapp BackEnd
+docker build -t mysql Database
+docker run -d --name=mysql -p 6603:3306 --network=private -v mysql:/var/lib/mysql mysql
+sleep 25
+docker run --name=clinicapp -p 8080:1234 --network=private clinicapp
